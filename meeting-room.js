a5b1468a1339f53e450a188dcd77eefeca67a4d6
varunmk17 <varunmk17@gmail.com>
2022-09-06 19:20:15 -0700

    Create meeting-room.js

---
+++ meeting-room.js
@@ -0,0 +1,25 @@
+const canAttendMeetings = (intervals = []) => {
+    if(!intervals || intervals.length < 1) return true;
+    
+    intervals.sort((a, b) => a[0] - b[0]);
+    let l = intervals.length;
+    let curr = [intervals[0][0], intervals[0][1]]
+    
+    for(let i = 1; i < l; i++) {
+        const [start, end] = curr;
+        const [nextStart, nextEnd] = intervals[i];
+        
+        if(nextStart < end) {
+            return false;
+        } else {
+            curr = [Math.min(start, nextStart), Math.max(end, nextEnd)]
+        }
+    }
+    
+    return true;
+}
+
+// let intervals = [[0,30],[5,10],[15,20]];
+let intervals = [[5,8],[9,15]]
+const res = canAttendMeetings(intervals);
+console.log(res)
