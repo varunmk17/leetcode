81a6e102362a6756e28b6ec3a4c27c94f74f754a
varunmk17 <varunmk17@gmail.com>
2022-09-10 23:22:52 -0700

    Update mergelogs.js

--- mergelogs.js
+++ mergelogs.js
@@ -1,9 +1,45 @@
 const mergeLogs = (logs1 = [], logs2 = []) => {
-    let res = [];
     let combinedLogs = [...logs1, ...logs2];
     
+    let map = combinedLogs.reduce((acc, curr) => {
+        let [version, ...rest] = curr.split(' ');
+        
+        if(acc[version]) {
+            let val = rest.slice(1).join(' ');
+            acc[version].push(val)
+        } else {
+            let val = [`-----`].concat(rest.slice(1)).join(' ');
+            acc[version] = [];
+            acc[version].push(val);
+        }
+
+        return acc;
+    }, {});
+    
+    const sortedVersions = Object.keys(map).sort(sortFn).reverse();
+    
+    const res = sortedVersions.reduce((acc, curr) => {
+        acc.push(`${curr} ${map[curr].join(', ')}`)
+        
+        return acc;
+    }, [])
     
     return res;
+}
+
+const sortFn = (version1, version2) => {
+    if(version1 === version2) return 0;
+    let arr1 = version1.split('.');
+    let arr2 = version2.split('.');
+    let l = Math.max(arr1.length, arr2.length);
+    
+    for(let i = 0; i < l; i++) {
+        let v1 = parseInt(arr1[i]);
+        let v2 = parseInt(arr2[i]);
+        
+        if(v1 < v2) return -1;
+        if(v1 > v2) return 1;
+    }
 }
 
 let logs1 = [
