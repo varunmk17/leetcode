77ea0c03e7b1ff628a64f476ee5c32f322bf972c
varunmk17 <varunmk17@gmail.com>
2022-09-10 18:29:30 -0700

    Update mergelogs.js

--- mergelogs.js
+++ mergelogs.js
@@ -1,21 +1,23 @@
 const mergeLogs = (logs1 = [], logs2 = []) => {
     let res = [];
+    let combinedLogs = [...logs1, ...logs2];
+    
     
     return res;
 }
 
 let logs1 = [
-    `10.9.0 ----- #comment 1`,
-    `5.1.0 ----- #comment 2`
-    `5.3.4 ----- #comment 3`,
-    `1.1.0 ----- #comment 4`
+    '10.9.0 ----- #comment 1',
+    '5.1.0 ----- #comment 2',
+    '5.3.4 ----- #comment 3',
+    '1.1.0 ----- #comment 4'
 ];
 
 let logs2 = [
-    `10.9.0 ----- #comment 5`,
-    `5.1.0 ----- #comment 6`
-    `6.3.9 ----- #comment 7`,
-    `1.0.0 ----- #comment 8`
+    '10.9.2 ----- #comment 5',
+    '5.1.0 ----- #comment 6',
+    '6.3.9 ----- #comment 7',
+    '1.0.0 ----- #comment 8'
 ];
 
 const res = mergeLogs(logs1, logs2);
