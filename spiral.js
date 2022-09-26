32c9cdca05d99621aa86a922c7027c513a2db60d
varunmk17 <varunmk17@gmail.com>
2022-09-26 16:17:07 -0700

    completed

--- spiral.js
+++ spiral.js
@@ -1,13 +1,68 @@
+const spiralFn = (row, col, size, counter, direction, result) => {
+    if(counter < 1) return result;
+    
+    result[row][col] = counter;
+    
+    switch (direction) {
+        case 'up':
+            if(row > 0 && result[row-1][col] == 0) {
+                row--;
+            } else {
+                direction = 'left';
+                col--;
+            }
+
+            counter--;
+            break;
+    
+        case 'left': 
+            if(col > 0 && result[row][col-1] == 0) {
+                col--;
+            } else {
+                direction = 'down';
+                row++;
+            }
+            
+            counter--;
+            break;
+            
+        case 'down': 
+            if(row < n-1 && result[row+1][col] == 0) {
+                row++;
+            } else {
+                direction = 'right';
+                col++;
+            }
+            
+            counter--;
+            break;
+            
+        case 'right': 
+             if(col < n-1 && result[row][col+1] == 0) {
+                col++;
+            } else {
+                direction = 'up';
+                row--;
+            }
+            
+            counter--;
+            break;
+    }
+    
+    spiralFn(row, col, size, counter, direction, result)
+}
+
 const spiral = (n) => {
     let res = Array(n).fill().map(x => Array(n).fill(0));
+    let row = n-1, col = n-1;
+    let counter = n*n;
     
-    let l = Math.floor(n/2), r = Math.floor(n/2);
-    res[l][r] = 1;
+    spiralFn(row, col, n, counter, "up", res);
     
     return res;
 }
 
-const n = 7;
+const n = 5;
 const res = spiral(n);
 console.log(res)
 
